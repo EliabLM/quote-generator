f1bab71b5299cc790c9c9be605261d122851ddd3
eliablm <eliablopez@hotmail.com>
2021-08-03 21:55:14 -0500

    Agregar funcionalidad de los botones

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 // ========== Local quotes ========== //
@@ -9,7 +15,22 @@ let apiQuotes = [];
 // Show new quote
 function newQuote() {
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote);
+
+	// Check Author field
+	if (!quote.author) {
+		authorText.textContent = "Unknown";
+	} else {
+		authorText.textContent = quote.author;
+	}
+
+	// Check quote length
+	if (quote.text.length > 120) {
+		quoteText.classList.add("long-quote");
+	} else {
+		quoteText.classList.remove("long-quote");
+	}
+
+	quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -24,6 +45,16 @@ async function getQuotes() {
 		alert(error);
 	}
 }
+
+// Tweet quote
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterUrl, "_blank");
+}
+
+// Event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // ON load
 // newLocalQuote();
